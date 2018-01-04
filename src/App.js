3b62e2e27a1636b68e19418798380c6e472dfb26
Timo <Timpzu@users.noreply.github.com>
2018-01-05 01:59:01 +0200

    Created mock server data
    Created mock server data as a placeholder for future real server data.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let defaultStyle = {
+  color: '#303030'
+};
+let mockServerData = {
+  user: {
+    name: 'Timo',
+    lists: [
+      {
+        name: 'First list',
+        items: ['This is the first item','and I am the second one']
+      },
+      {
+        name: 'List number 2',
+        items: ['Hello, I am first','Hi I am two']
+      }
+    ]
+  }
+};
 
 class Card extends Component {
   render() {
     return(
       <div>
-        <h2>Name</h2>
-        <p>Arvosana</p>
+        <h2 style={defaultStyle}>{this.props.lists && this.props.lists.length} items</h2>
+        <p style={{...defaultStyle, fontStyle: 'italic'}}>adaeae</p>
       </div>
     );
   }
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {serverData: {}}
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({serverData: mockServerData});
+    }, 1000);
+  }
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
-        <Card/>
-        <Card/>
+        {this.state.serverData.user ?
+          <div>
+            <h1>{this.state.serverData.user.name}'s App</h1>
+            <Card lists={this.state.serverData.user.lists}/>
+            <Card/>
+          </div> : <h4>Loading content...</h4>
+        }
       </div>
     );
   }
 }
-
 export default App;
