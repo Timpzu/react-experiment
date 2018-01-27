749a3caec4a7ab70554aa70d230b65fdab5e405c
timpzu <Timpzu@users.noreply.github.com>
2018-01-27 18:14:01 +0200

    Created weather card UI elements

--- src/App.js
+++ src/App.js
@@ -1,87 +1,118 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { Row, Column, Menu, MenuItem, MenuText, TopBar } from 'react-foundation';
-import { ComposableMap, ZoomableGroup, Geographies, Geography } from "react-simple-maps"
 import './foundation.css';
 import './App.css';
 
-let defaultStyle = {
-  color: '#303030'
-};
-let mockServerData = {
-  user: {
-    name: 'Timo',
-    lists: [
-      {
-        name: 'First list',
-        items: ['This is the first item','and I am the second one']
-      },
-      {
-        name: 'List number 2',
-        items: ['Hello, I am first','Hi I am two']
-      }
-    ]
-  }
-};
-
-class NavBar extends Component {
-  render() {
-    return (
-      <TopBar>
-        <Menu>
-          <MenuText>Weather app</MenuText>
-          <MenuItem><a>Valinta 1</a></MenuItem>
-          <MenuItem><a>Valinta 2</a></MenuItem>
-        </Menu>
-      </TopBar>
-    )
-  }
+let cityCardStyle = {
+  width: '100%',
+  background: '#fff',
+  boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+  borderRadius: '4px',
 }
-class weatherMap extends Component {
-    render() {
-      return(
-        <span></span>
-      )
+let infoWindowStyle = {
+  height: '250px',
+  width:'100%',
+  padding: '10px',
+  background: '',
+  color: '#fff',
+  textAlign: 'center'
+}
+let columnStyle = {
+  paddingTop: '0.9375rem'
+}
+const night = 'linear-gradient(to bottom, #1e3c72 0%, #1e3c72 1%, #2a5298 100%)';
+const morning = 'linear-gradient(120deg, #f6d365 0%, #fda085 100%)';
+const day = 'linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)';
+const evening = 'linear-gradient(to bottom, #a18cd1 0%, #fbc2eb 100%)';
+
+let mockserverData = {
+  cities: [
+    {
+      name: 'Helsinki',
+      latestNotes: [],
+      timeZone: 2
+    },
+    {
+      name: 'Tokyo',
+      latestNotes: [],
+      timeZone: 9
+    },
+    {
+      name: 'New York',
+      latestNotes: [],
+      timeZone: -5
+    },
+    {
+      name: 'Dubai',
+      latestNotes: [],
+      timeZone: 4
+    },
+    {
+      name: 'Amsterdam',
+      latestNotes: [],
+      timeZone: 1
     }
+  ]
 }
 
-class Card extends Component {
+class CityCard extends Component {
   render() {
-    let propsList = this.props.list;
-    return(
-      <div>
-        <h2 style={defaultStyle}>{propsList.name}</h2>
-        <ul style={{...defaultStyle, fontStyle: 'italic', listStyle: 'none', padding: '0'}}>
-          {propsList.items.map(item =>
-            <li>{item}</li>
+    const cityProps = this.props.city;
+    let date = new Date();
+    date.setHours(date.getUTCHours() + cityProps.timeZone);
+    let currentHour = date.getHours();
+
+    currentHour >= 22 || currentHour < 6
+    ? infoWindowStyle.background = night : infoWindowStyle.background = day;
+
+    return (
+      <div style={cityCardStyle}>
+        <div style={{...infoWindowStyle}}>
+          <h4>{cityProps.name}</h4>
+        </div>
+        <ul>
+          {cityProps.latestNotes.map(info =>
+            <li></li>
           )}
         </ul>
       </div>
-    );
+    )
   }
 }
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {serverData: {}}
   }
   componentDidMount() {
     setTimeout(() => {
-      this.setState({serverData: mockServerData});
+      this.setState({serverData: mockserverData});
     }, 1000);
   }
   render() {
     return (
       <div className="App">
-        <NavBar/>
-        {this.state.serverData.user ?
-          <div>
-            <h1>{this.state.serverData.user.name}'s App</h1>
-            {this.state.serverData.user.lists.map(list =>
-              <Card list={list}/>
-            )}
-          </div> : <h4>Loading content...</h4>
-        }
+        <TopBar>
+          <Menu>
+            <MenuText>Weather app</MenuText>
+            <MenuItem><a>Valinta 1</a></MenuItem>
+            <MenuItem><a>Valinta 2</a></MenuItem>
+          </Menu>
+        </TopBar>
+        <main style={{backgroundColor:'#F4F7F5', marginTop:'54px'}}>
+          {this.state.serverData.cities ?
+            <row className="clearfix">
+              {this.state.serverData.cities.map(city =>
+                <Column style={columnStyle} small={12} large={3}>
+                  <CityCard city={city}/>
+                </Column>
+              )}
+            </row>: <h4>Loading content...</h4>
+
+          }
+        </main>
       </div>
     );
   }
