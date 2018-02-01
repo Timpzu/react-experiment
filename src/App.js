3b48f3ebb2519e2e2ea6a59486e87b738de787c0
timpzu <Timpzu@users.noreply.github.com>
2018-02-01 14:54:38 +0200

    Installed & initialized Firebase database

--- src/App.js
+++ src/App.js
@@ -1,117 +1,66 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Row, Column, Menu, MenuItem, MenuText, TopBar } from 'react-foundation';
-import './foundation.css';
-import './App.css';
+import { Row, Column, Menu, MenuItem, MenuText, TopBar, Button } from 'react-foundation';
+import './css/foundation.css';
+import './css/App.css';
+import firebase from 'firebase';
+import { firebaseConfig } from './firebase.js';
+import Card from './Card.js';
 
-let cityCardStyle = {
-  width: '100%',
-  background: '#fff',
-  boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
-  borderRadius: '4px',
-}
-let infoWindowStyle = {
-  height: '250px',
-  width:'100%',
-  padding: '10px',
-  background: '',
-  color: '#fff',
-  textAlign: 'center'
+firebase.initializeApp(firebaseConfig);
+
+let bodyStyle = {
+  background: '#eee',
+  minHeight: 'calc(100vh - 54px)'
 }
 let columnStyle = {
   paddingTop: '0.9375rem'
 }
-const night = 'linear-gradient(to bottom, #1e3c72 0%, #1e3c72 1%, #2a5298 100%)';
-const morning = 'linear-gradient(120deg, #f6d365 0%, #fda085 100%)';
-const day = 'linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%)';
-const evening = 'linear-gradient(to bottom, #a18cd1 0%, #fbc2eb 100%)';
-
-let mockserverData = {
-  cities: [
-    {
-      name: 'Helsinki',
-      latestNotes: [],
-      timeZone: 2
-    },
-    {
-      name: 'Tokyo',
-      latestNotes: [],
-      timeZone: 9
-    },
-    {
-      name: 'New York',
-      latestNotes: [],
-      timeZone: -5
-    },
-    {
-      name: 'Dubai',
-      latestNotes: [],
-      timeZone: 4
-    },
-    {
-      name: 'Amsterdam',
-      latestNotes: [],
-      timeZone: 1
-    }
-  ]
-}
-
-class CityCard extends Component {
-  render() {
-    const cityProps = this.props.city;
-    let date = new Date();
-    date.setHours(date.getUTCHours() + cityProps.timeZone);
-    let currentHour = date.getHours();
-
-    currentHour >= 22 || currentHour < 6
-    ? infoWindowStyle.background = night : infoWindowStyle.background = day;
-
-    return (
-      <div style={cityCardStyle}>
-        <div style={{...infoWindowStyle}}>
-          <h4>{cityProps.name}</h4>
-        </div>
-        <ul>
-          {cityProps.latestNotes.map(info =>
-            <li></li>
-          )}
-        </ul>
-      </div>
-    )
-  }
-}
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {serverData: {}}
+  constructor() {
+    super();
+    this.state = {
+      weatherCards: []
+    }
   }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({serverData: mockserverData});
-    }, 1000);
+    const observatoriesRef = firebase.database().ref('observatories');
+    observatoriesRef.on('value', (snapshot) => {
+      let cards = snapshot.val();
+      let newState = [];
+      for (let i in cards) {
+        newState.push({
+          id: i,
+          location: cards[i].location
+        });
+      }
+      this.setState({
+        weatherCards: newState
+      });
+    });
+
   }
+
   render() {
     return (
-      <div className="App">
+      <div style={bodyStyle} className="App">
         <TopBar>
           <Menu>
             <MenuText>Weather app</MenuText>
-            <MenuItem><a>Valinta 1</a></MenuItem>
-            <MenuItem><a>Valinta 2</a></MenuItem>
           </Menu>
         </TopBar>
-        <main style={{backgroundColor:'#F4F7F5', marginTop:'54px'}}>
-          {this.state.serverData.cities ?
-            <row className="clearfix">
-              {this.state.serverData.cities.map(city =>
-                <Column style={columnStyle} small={12} large={3}>
-                  <CityCard city={city}/>
+        <main style={{marginTop:'54px'}}>
+          <row className="clearfix">
+            {this.state.weatherCards.map((observatory) => {
+              return(
+                <Column style={columnStyle} small={12} medium={6} large={3}>
+                  <Card observatory={observatory}/>
                 </Column>
-              )}
-            </row>: <h4>Loading content...</h4>
-
-          }
+              )
+            })}
+          </row>
         </main>
       </div>
     );
---
+++ src/Card.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { Button } from 'react-foundation';
+import firebase from 'firebase';
+
+let cardStyle = {
+  width: '100%',
+  background: '#fff',
+  boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+  borderRadius: '4px',
+  padding: '10px'
+}
+
+export default class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: '',
+      weather: '',
+      temperature: 0,
+      entries: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const observatoriesRef = firebase.database().ref('observatories').child(this.props.observatory.id);
+    const entriesRef = observatoriesRef.child('entries');
+    const currentWeather = {
+      weather: this.state.weather,
+      temperature: this.state.temperature
+    }
+    entriesRef.push(currentWeather);
+    this.setState({
+      weather: '',
+      temperature: 0
+    });
+  }
+
+  componentDidMount() {
+    const observatoriesRef = firebase.database().ref('observatories').child(this.props.observatory.id);
+    const entriesRef = observatoriesRef.child('entries');
+
+    entriesRef.on('value', (snapshot) => {
+      let currentWeather = snapshot.val();
+      let newState = [];
+      for (let i in currentWeather) {
+        newState.push({
+          id: i,
+          weather: currentWeather[i].weather,
+          temperature: currentWeather[i].temperature
+        });
+      }
+      this.setState({
+        entries: newState
+      });
+    });
+    this.setState({location: this.props.observatory.location})
+  }
+  render() {
+    return (
+      <article style={cardStyle}>
+        <h2>{this.state.location}</h2>
+        {this.state.entries.map((weather) => {
+          return(
+            <ul>
+              <li>{weather.weather}</li>
+              <li>{weather.temperature}</li>
+            </ul>
+          )
+        })}
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="weather" placeholder="Weather description" onChange={this.handleChange} value={this.state.weather} />
+          <input type="number" name="temperature" placeholder="Weather in Celsius" onChange={this.handleChange} value={this.state.temperature} />
+          <Button>Update</Button>
+        </form>
+      </article>
+    );
+  }
+}
