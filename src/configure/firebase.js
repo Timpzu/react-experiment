1e3c9f2656e94965321ebdfe1860376d283f39ef
timpzu <Timpzu@users.noreply.github.com>
2018-02-08 21:19:43 +0200

    Updated .gitignore

---
+++ src/configure/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCeEjbQZMNkgLxN206TcrvWxLWxsV0Qk2c",
+    authDomain: "timpantieteenlaitos.firebaseapp.com",
+    databaseURL: "https://timpantieteenlaitos.firebaseio.com",
+    projectId: "timpantieteenlaitos",
+    storageBucket: "timpantieteenlaitos.appspot.com",
+    messagingSenderId: "1027923203738"
+  };
