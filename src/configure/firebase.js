664b56724ebccee4e37c5741381c78a72d31cc5e
timpzu <Timpzu@users.noreply.github.com>
2018-02-08 19:50:32 +0200

    Added configure files

---
+++ src/configure/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAQvfTe8QCTFxbKCS7AJS5c7eM07fp9L6k",
+    authDomain: "weather-app-80731.firebaseapp.com",
+    databaseURL: "https://weather-app-80731.firebaseio.com",
+    projectId: "weather-app-80731",
+    storageBucket: "weather-app-80731.appspot.com",
+    messagingSenderId: "788327155137"
+  };
